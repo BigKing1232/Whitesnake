d34b6441ab1497092d54843a7c77a5b526d6a97a
BigKing1232 <149486226+BigKing1232@users.noreply.github.com>
2023-10-31 15:47:27 +0300

    Create WhitesnakeEntity.java

---
+++ src /main/java/com/bigking123/rotp_whitesnake/entity/stand/stands/WhitesnakeEntity.java
@@ -0,0 +1,13 @@
+package com.bigking123.rotp_whitesnake.entity.stand.stands;
+
+import com.github.standobyte.jojo.entity.stand.StandEntity;
+import com.github.standobyte.jojo.entity.stand.StandEntityType;
+
+import net.minecraft.world.World;
+
+public class WhitesnakeEntity extends StandEntity {
+    
+    public WhitesnakeEntity(StandEntityType<WhitesnakeEntity> type, World world) {
+        super(type, world);
+    }
+}
